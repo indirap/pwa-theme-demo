8b7d9cd0ca58663158e166e69b79ef681e9704c7
Indira Pranabudi <ipranabudi@vistaprint.com>
2019-09-23 08:59:13 -0400

    Add theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,12 +23,13 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `@indirap/gatsby-theme-indira-1`
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query SiteTitleQueryRoot {
       site {
         siteMetadata {
           title
